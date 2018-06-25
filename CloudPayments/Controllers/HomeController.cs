579891e92333a0bc1e6b9b61935a6ebb4253d606
Fedor <novikovfedor@gmail.com>
2018-06-26 00:40:38 +0300

    Base realization of js

--- CloudPayments/Controllers/HomeController.cs
+++ CloudPayments/Controllers/HomeController.cs
@@ -6,22 +6,25 @@ using System.Threading.Tasks;
 using CloudPayments.DataServices;
 using Microsoft.AspNetCore.Mvc;
 using CloudPayments.Models;
+using CloudPayments.Services;
 
 namespace CloudPayments.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IProductsRepository _productsRepository;
+        private readonly ICurrencyManager _currencyManager;
 
-        public HomeController(IProductsRepository productsRepository)
+        public HomeController(IProductsRepository productsRepository, ICurrencyManager currencyManager)
         {
             _productsRepository = productsRepository;
+            _currencyManager = currencyManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var products = await _productsRepository.ListAsync();
-            return View(products);
+            return View(products.Select(p => new ProductViewModel(p, _currencyManager)));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- CloudPayments/Models/Product.cs
+++ CloudPayments/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using CloudPayments.Services;
 
 namespace CloudPayments.Models
 {
@@ -14,5 +15,23 @@ namespace CloudPayments.Models
         public double Price { get; set; }
         public string ImageName { get; set; }
         public string Currency { get; set; }
+    }
+
+    public class ProductViewModel
+    {
+        private readonly Product _product;
+        private readonly ICurrencyManager _currencyManager;
+        public int Id => _product.Id;
+        public string Title => _product.Title;
+
+        public double Price => _product.Price;
+        public string ImagePath => "/images/" + _product.ImageName;
+        public string Currency => _currencyManager.ParseCurrency(_product.Currency).Code;
+
+        public ProductViewModel(Product product, ICurrencyManager currencyManager)
+        {
+            _product = product;
+            _currencyManager = currencyManager;
+        }
     }
 }
